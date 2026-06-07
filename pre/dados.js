11e5eba01ab7fb648b7897a42fbc01bff9488db8
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-06-08 00:41:44 +0100

    Update dados.js

--- pre/dados.js
+++ pre/dados.js
@@ -21,10 +21,10 @@ const JOGO_CONFIG = {
     },
     
     listaJogos: [
-        { nome: "As Metades", icon: "metades_peixe.png", link: "metades_peixe/", jogadas: 0 },
-        { nome: "Vamos Contar Metades", icon: "metades_contar.png", link: "metades_contar/", jogadas: 0 },
-        { nome: "Formas Metades", icon: "metades_formas.png", link: "metades_formas/", jogadas: 0 },
-        { nome: "Cores Mágicas Metades", icon: "metades_cores.png", link: "metades_cores/", jogadas: 0 },
+        { nome: "As Metades", icon: "aaaa", link: "metades_peixe/", jogadas: 0 },
+        { nome: "Vamos Contar Metades", icon: "aaaa.png", link: "metades_contar/", jogadas: 0 },
+        { nome: "Formas Metades", icon: "aaaa.png", link: "metades_formas/", jogadas: 0 },
+        { nome: "Cores Mágicas Metades", icon: "aaaa.png", link: "metades_cores/", jogadas: 0 },
         { nome: "Objetos Metades", icon: "metades_objetos.png", link: "metades_objetos/", jogadas: 0 },
         { nome: "Transportes Metades", icon: "metades_transportes.png", link: "metades_transportes/", jogadas: 0 },
         { nome: "Animais Metades", icon: "metades_animais.png", link: "metades_animais/", jogadas: 0 },
