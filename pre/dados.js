04ed71270ed81301bec523e390060ec9ca6867a0
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-06-08 00:34:31 +0100

    Update dados.js

--- pre/dados.js
+++ pre/dados.js
@@ -21,13 +21,13 @@ const JOGO_CONFIG = {
     },
     
     listaJogos: [
-        { nome: "As Metades", icon: "aaaa.png", link: "metades_peixe/", jogadas: 21 },
-        { nome: "Vamos Contar Metades", icon: "aaaa.png", link: "metades_contar/", jogadas: 42 },
-        { nome: "Formas Metades", icon: "aaaa.png", link: "metades_formas/", jogadas: 18 },
-        { nome: "Cores Mágicas Metades", icon: "aaaa.png", link: "metades_cores/", jogadas: 35 },
-        { nome: "Objetos Metades", icon: "aaaa.png", link: "metades_objetos/", jogadas: 28 },
-        { nome: "Transportes Metades", icon: "aaaa.png", link: "metades_transportes/", jogadas: 15 },
-        { nome: "Animais Metades", icon: "aaaa.png", link: "metades_animais/", jogadas: 50 },
-        { nome: "Metades Divertidas", icon: "aaaa.png", link: "metades_divertidas/", jogadas: 22 }
+        { nome: "As Metades", icon: "metades_peixe.png", link: "metades_peixe/", jogadas: 0 },
+        { nome: "Vamos Contar Metades", icon: "metades_contar.png", link: "metades_contar/", jogadas: 0 },
+        { nome: "Formas Metades", icon: "metades_formas.png", link: "metades_formas/", jogadas: 0 },
+        { nome: "Cores Mágicas Metades", icon: "metades_cores.png", link: "metades_cores/", jogadas: 0 },
+        { nome: "Objetos Metades", icon: "metades_objetos.png", link: "metades_objetos/", jogadas: 0 },
+        { nome: "Transportes Metades", icon: "metades_transportes.png", link: "metades_transportes/", jogadas: 0 },
+        { nome: "Animais Metades", icon: "metades_animais.png", link: "metades_animais/", jogadas: 0 },
+        { nome: "Metades Divertidas", icon: "metades_divertidas.png", link: "metades_divertidas/", jogadas: 0 }
     ]
 };
