197c1f6a7a6aeb9096a27b6302192b933ad18cb9
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-07-18 19:21:39 +0100

    Update dados.js

--- 1/dados.js
+++ 1/dados.js
@@ -15,7 +15,7 @@ const JOGO_CONFIG = {
         ano2: "icon2.png",
         ano3: "icon3.png",
         ano4: "icon4.png",
-        voltar: "voltar_az.png",
+        voltar: "seta.png",
         voltarMobile: "voltar_az.png" 
     },
     links: {
