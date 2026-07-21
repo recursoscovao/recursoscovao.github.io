9a2fa64387c89c68713516df59b2aef4563c3764
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-07-21 18:43:06 +0100

    Update dados.js

--- 2/dados.js
+++ 2/dados.js
@@ -1,13 +1,11 @@
 const JOGO_CONFIG = {
-    // MODIFICA AQUI: "pre", "ano1", "ano2", "ano3" ou "ano4"
     anoAtivo: "ano2", 
-
     textos: {
         tituloLinha1: "JOVENS",
         tituloLinha2: "EXPLORADORES",
-        subtitulo: "ATIVIDADES | 2º ANO", // Podes automatizar isto no JS se quiseres
+        subtitulo: "ATIVIDADES | 2º ANO",
         intro: "O que vamos aprender hoje?",
-        rodape: "&copy; Jovens Exploradores - Recursos Educativos"
+        rodape: "&copy; jovens Exploradores - Recursos Educativos"
     },
     caminhoIconsMenu: "../icons/", 
     caminhoIconsJogos: "../icons/", 
@@ -15,18 +13,19 @@ const JOGO_CONFIG = {
         home: "home.png",
         pre: "iconpre.png",
         ano1: "icon1.png",
-        ano2: "icon2.png", // Ícone da tartaruga
+        ano2: "icon2.png", 
         ano3: "icon3.png",
         ano4: "icon4.png",
-        voltar: "voltar_az.png"
+        voltar: "voltar_az.png",
+        controller: "jogo.png"
     },
     links: {
-        home: "../", 
-        pre: "../pre/",
-        ano1: "../1/",
-        ano2: "./",
-        ano3: "../3/",
-        ano4: "../4/"
+        home: "../index.html", 
+        pre: "../pre/index.html",
+        ano1: "./index.html",
+        ano2: "../2/index.html",
+        ano3: "../3/index.html",
+        ano4: "../4/index.html"
     },
     temaCores: {
         azul: { borda: "#5ba4e5", fundo: "#e1f0ff", texto: "#0066ff" },
@@ -37,5 +36,14 @@ const JOGO_CONFIG = {
         { nome: "Português", icon: "pt.png", link: "pt/", tema: "azul" },
         { nome: "Matemática", icon: "mat.png", link: "mat/", tema: "verde" },
         { nome: "Estudo do Meio", icon: "em.png", link: "em/", tema: "castanho" },
+    ],
+    // ADICIONE ESTA LISTA DE DESTAQUES
+    listaDestaques: [
+        { nome: "Quebra-Cabeças", icon: "puzzle.png", link: "jogos/puzzle.html", estrelas: 4 },
+        { nome: "Memória", icon: "memory.png", link: "jogos/memoria.html", estrelas: 5 },
+        { nome: "Labirintos", icon: "maze.png", link: "jogos/labirintos.html", estrelas: 4 },
+        { nome: "Desafios", icon: "trophy.png", link: "jogos/desafios.html", estrelas: 5 },
+        { nome: "Aventuras", icon: "rocket.png", link: "jogos/aventuras.html", estrelas: 5 },
+        { nome: "Pontuação", icon: "target.png", link: "jogos/pontos.html", estrelas: 5 }
     ]
 };
