beed0a6ee78ec618e8c814838b0df526613441c4
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-07-23 10:17:09 +0100

    Update dados.js

--- teste1/dados.js
+++ teste1/dados.js
@@ -1,42 +1,58 @@
 // === CONFIGURAÇÃO MESTRE ===
 const CONFIG_MESTRE = {
-    ano: "ano1",        
-    area: "portugues",  
+    ano: "pre",        
+    area: "pre",  // Altera para "portugues", "matematica", "estudo" ou "pre"
     nomeJogo: "Chuva de Letras",
-    categoriaAtiva: "maiusculas" // Define qual categoria carregar ao iniciar
+    categoriaAtiva: "maiusculas" 
 };
 
 // === CATEGORIAS DO JOGO ===
 const JOGO_CATEGORIAS = {
     maiusculas: {
         nome: "Letras Maiúsculas",
-        descricao: "Clica nas letras maiúsculas que aparecem!",
-        imgCapa: "vogal_maiuscula.png",
+        descricao: "Encontra a letra igual à que aparece em cima.",
+        imgCapa: "https://cdn-icons-png.flaticon.com/512/388/388451.png", // Exemplo Bee/Abelha
         letras: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
         tipo: "upper"
     },
     minusculas: {
         nome: "Letras Minúsculas",
-        descricao: "Clica nas letras minúsculas que aparecem!",
-        imgCapa: "vogal_minuscula.png",
+        descricao: "Encontra a letra igual à que aparece em cima.",
+        imgCapa: "https://cdn-icons-png.flaticon.com/512/388/388451.png",
         letras: "abcdefghijklmnopqrstuvwxyz".split(""),
         tipo: "lower"
     }
 };
 
-// === BIBLIOTECA DE TEMAS ===
+// === BIBLIOTECA DE TEMAS (CORES DA IMAGEM) ===
 const BIBLIOTECA_TEMAS = {
     "portugues": { 
-        corPagina: "#f0f4f8", corPrimaria: "#5ba4e5", corEscura: "#3d7db8", corTexto: "#5d7082", voltarMobile: "voltar_az.png" 
+        corPagina: "#e9f2fb",      
+        corCard: "#f0f7ff",        
+        corPrimaria: "#5ba4e5",    
+        corEscura: "#3d7db8", 
+        voltarMobile: "voltar_az.png" 
     },
     "matematica": { 
-        corPagina: "#f0fdf4", corPrimaria: "#45cfa8", corEscura: "#2BA886", corTexto: "#45cfa8", voltarMobile: "voltar_vr.png"
+        corPagina: "#e9fbf4", 
+        corCard: "#f0fdf9",        
+        corPrimaria: "#45cfa8",    
+        corEscura: "#2BA886", 
+        voltarMobile: "voltar_vr.png"
     },
     "estudo": { 
-        corPagina: "#fdf2f2", corPrimaria: "#994D4D", corEscura: "#6C3737", corTexto: "#994D4D", voltarMobile: "voltar_cs.png"
+        corPagina: "#fef5e7", 
+        corCard: "#fffaf2",        
+        corPrimaria: "#ed9d3e",    
+        corEscura: "#c57e2d", 
+        voltarMobile: "voltar_cs.png"
     },
     "pre": { 
-        corPagina: "#fff5f7", corPrimaria: "#E691A7", corEscura: "#D54267", corTexto: "#E691A7", voltarMobile: "voltar_rs.png"
+        corPagina: "#fff5f7", 
+        corCard: "#ffffff", 
+        corPrimaria: "#E691A7", 
+        corEscura: "#D54267", 
+        voltarMobile: "voltar_rs.png"
     }
 };
 
@@ -47,35 +63,25 @@ const BIBLIOTECA_CONTEUDO = {
     },
     "ano1": {
         "portugues": { t1: "PEQUENOS", t2: "LEITORES", sub: "Português | 1º Ano", rodape: "© Pequenos Curiosos 2024" },
-        "matematica": { t1: "PEQUENOS", t2: "MATEMÁTICOS", sub: "Matemática | 1º Ano", rodape: "© Pequenos Curiosos 2024" }
+        "matematica": { t1: "PEQUENOS", t2: "MATEMÁTICOS", sub: "Matemática | 1º Ano", rodape: "© Pequenos Curiosos 2024" },
+        "estudo": { t1: "PEQUENOS", t2: "EXPLORADORES", sub: "Estudo do Meio | 1º Ano", rodape: "© Pequenos Curiosos 2024" }
     }
 };
 
-// === CONFIGURAÇÕES GERAIS DO JOGO ===
+// === CONFIGURAÇÕES GERAIS ===
 const JOGO_CONFIG = {
     linkVoltar: "../",
     caminhoImg: "../../img/",    
     caminhoIcons: "../../icons/", 
     totalRondas: 10,
-    
     sons: {
         acerto: "https://cdn.pixabay.com/audio/2021/08/04/audio_0625c1539c.mp3",
         erro: "https://cdn.pixabay.com/audio/2022/03/10/audio_c330c67761.mp3",
         vitoria: "https://cdn.pixabay.com/audio/2024/02/07/audio_293963428f.mp3"
     },
-
-    iconesMenu: { 
-        home: "home.png", pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" 
-    },
-
-    links: { 
-        home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1"
-    },
-    
+    iconesMenu: { home: "home.png", pre: "iconpre.png", ano1: "icon1.png" },
     relatorios: [
-        { min: 90, max: 100, titulo: "És um craque!", img: "taca_1.png" },
-        { min: 70, max: 89, titulo: "Muito bem!", img: "taca_2.png" },
-        { min: 50, max: 69, titulo: "Estás quase lá!", img: "taca_3.png" },
-        { min: 0, max: 49, titulo: "Continua a tentar!", img: "taca_4.png" }
+        { min: 90, max: 100, titulo: "És um craque!", img: "https://cdn-icons-png.flaticon.com/512/3112/3112946.png" },
+        { min: 0, max: 89, titulo: "Muito bem!", img: "https://cdn-icons-png.flaticon.com/512/3112/3112946.png" }
     ]
 };
