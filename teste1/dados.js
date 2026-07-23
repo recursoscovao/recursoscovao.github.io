dd65caf63f740f1ef91106d3b0c292ecbac33b3e
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-07-23 10:56:27 +0100

    Update dados.js

--- teste1/dados.js
+++ teste1/dados.js
@@ -2,42 +2,47 @@
 const CONFIG_MESTRE = {
     ano: "pre",        
     area: "pre", // "pre", "portugues", "matematica", "estudo"
-    nomeJogo: "Encontra a Abelha",
-    categoriaAtiva: "maiusculas" 
+    nomeJogo: "Encontra o Par",
+    categoriaAtiva: "animais" 
 };
 
 // === CATEGORIAS DO JOGO ===
 const JOGO_CATEGORIAS = {
     maiusculas: {
         nome: "Letras Maiúsculas",
-        descricao: "Encontra o desenho igual ao de cima.",
+        descricao: "Encontra a letra igual à de cima.",
         imgCapa: "https://cdn-icons-png.flaticon.com/512/388/388451.png",
-        letras: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+        itens: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map(l => ({ id: l, valor: l, tipo: 'texto' })),
         tipo: "upper"
+    },
+    animais: {
+        nome: "Animais Curiosos",
+        descricao: "Observa com atenção e encontra o par igual!",
+        imgCapa: "https://cdn-icons-png.flaticon.com/512/809/809052.png", // Abelha
+        itens: [
+            { id: 1, valor: "https://cdn-icons-png.flaticon.com/512/822/822102.png", tipo: 'img' }, // Arara
+            { id: 2, valor: "https://cdn-icons-png.flaticon.com/512/3069/3069172.png", tipo: 'img' }, // Elefante
+            { id: 3, valor: "https://cdn-icons-png.flaticon.com/512/2622/2622055.png", tipo: 'img' }, // Jacaré
+            { id: 4, valor: "https://cdn-icons-png.flaticon.com/512/3069/3069186.png", tipo: 'img' }, // Coala
+            { id: 5, valor: "https://cdn-icons-png.flaticon.com/512/1141/1141771.png", tipo: 'img' }, // Caranguejo
+            { id: 6, valor: "https://cdn-icons-png.flaticon.com/512/3069/3069209.png", tipo: 'img' }, // Gorila
+            { id: 7, valor: "https://cdn-icons-png.flaticon.com/512/427/427503.png", tipo: 'img' },  // Águia
+            { id: 8, valor: "https://cdn-icons-png.flaticon.com/512/809/809052.png", tipo: 'img' }   // Abelha
+        ]
     }
 };
 
 // === BIBLIOTECA DE TEMAS ===
 const BIBLIOTECA_TEMAS = {
-    "portugues": { 
-        corPagina: "#f0f7ff", corCard: "#e8f1f9", corPrimaria: "#5ba4e5", corEscura: "#3d7db8", corHamburger: "#5ba4e5", voltarMobile: "voltar_az.png" 
-    },
-    "matematica": { 
-        corPagina: "#f0fdf9", corCard: "#e8fbf4", corPrimaria: "#45cfa8", corEscura: "#2BA886", corHamburger: "#45cfa8", voltarMobile: "voltar_vr.png"
-    },
-    "estudo": { 
-        corPagina: "#fffaf2", corCard: "#fef6e8", corPrimaria: "#ed9d3e", corEscura: "#c57e2d", corHamburger: "#ed9d3e", voltarMobile: "voltar_cs.png"
-    },
-    "pre": { 
-        corPagina: "#fff5f7", corCard: "#ffffff", corPrimaria: "#E691A7", corEscura: "#D54267", corHamburger: "#E691A7", voltarMobile: "voltar_rs.png"
-    }
+    "portugues": { corPagina: "#f0f7ff", corCard: "#e8f1f9", corPrimaria: "#5ba4e5", corEscura: "#3d7db8", corHamburger: "#5ba4e5", voltarMobile: "voltar_az.png" },
+    "matematica": { corPagina: "#f0fdf9", corCard: "#e8fbf4", corPrimaria: "#45cfa8", corEscura: "#2BA886", corHamburger: "#45cfa8", voltarMobile: "voltar_vr.png" },
+    "estudo": { corPagina: "#fffaf2", corCard: "#fef6e8", corPrimaria: "#ed9d3e", corEscura: "#c57e2d", corHamburger: "#ed9d3e", voltarMobile: "voltar_cs.png" },
+    "pre": { corPagina: "#fff5f7", corCard: "#ffffff", corPrimaria: "#E691A7", corEscura: "#D54267", corHamburger: "#E691A7", voltarMobile: "voltar_rs.png" }
 };
 
 // === BIBLIOTECA DE CONTEÚDO (COMPLETA) ===
 const BIBLIOTECA_CONTEUDO = {
-    "pre": {
-        "pre": { t1: "PEQUENOS", t2: "CURIOSOS", sub: "Atividades | Pré-Escolar", rodape: "© Pequenos Curiosos 2024" }
-    },
+    "pre": { "pre": { t1: "PEQUENOS", t2: "CURIOSOS", sub: "Atividades | Pré-Escolar", rodape: "© Pequenos Curiosos 2024" } },
     "ano1": {
         "portugues": { t1: "PEQUENOS", t2: "LEITORES", sub: "Português | 1º Ano", rodape: "© Pequenos Leitores 2024" },
         "matematica": { t1: "PEQUENOS", t2: "MATEMÁTICOS", sub: "Matemática | 1º Ano", rodape: "© Pequenos Matemáticos 2024" },
@@ -70,12 +75,8 @@ const JOGO_CONFIG = {
         erro: "https://cdn.pixabay.com/audio/2022/03/10/audio_c330c67761.mp3",
         vitoria: "https://cdn.pixabay.com/audio/2024/02/07/audio_293963428f.mp3"
     },
-    iconesMenu: { 
-        home: "home.png", pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" 
-    },
-    links: { 
-        home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4" 
-    },
+    iconesMenu: { home: "home.png", pre: "iconpre.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png" },
+    links: { home: "/jogos", pre: "/jogos/pre", ano1: "/jogos/1", ano2: "/jogos/2", ano3: "/jogos/3", ano4: "/jogos/4" },
     relatorios: [
         { min: 90, max: 100, titulo: "És um craque!", img: "https://cdn-icons-png.flaticon.com/512/3112/3112946.png" },
         { min: 0, max: 89, titulo: "Muito bem!", img: "https://cdn-icons-png.flaticon.com/512/3112/3112946.png" }
