8ac63dd256b8a0020c45986f617e67adb33a3c56
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-07-26 11:11:22 +0100

    Update dados.js

--- teste3/dados.js
+++ teste3/dados.js
@@ -1,46 +1,119 @@
+// ==========================================
+// BIBLIOTECA GLOBAL (Temas e Textos)
+// ==========================================
+
 const BIBLIOTECA_TEMAS = {
-    "portugues": { corPagina: "#e9f0f8", corPrimaria: "#5ba4e5", corEscura: "#3d7db8", voltarMobile: "voltar_az.png" },
-    "matematica": { corPagina: "#e8f9f4", corPrimaria: "#45cfa8", corEscura: "#2BA886", voltarMobile: "voltar_vr.png" },
-    "estudo": { corPagina: "#EAE2E5", corPrimaria: "#994D4D", corEscura: "#6C3737", voltarMobile: "voltar_cs.png" },
-    "pre": { corPagina: "#FFF5F7", corPrimaria: "#E691A7", corEscura: "#D54267", voltarMobile: "voltar_rs.png" },
-    "jd": { corPagina: "#f0f2f5", corPrimaria: "#6c757d", corEscura: "#495057", voltarMobile: "voltar_cin.png" }
+    "portugues": { corPagina: "#e9f0f8", corPrimaria: "#5ba4e5", corEscura: "#3d7db8", corTexto: "#5d7082", voltarMobile: "voltar_az.png", corHamburger: "#5ba4e5" },
+    "matematica": { corPagina: "#e8f9f4", corPrimaria: "#45cfa8", corEscura: "#2BA886", corTexto: "#45cfa8", voltarMobile: "voltar_vr.png", corHamburger: "#45cfa8" },
+    "estudo": { corPagina: "#EAE2E5", corPrimaria: "#994D4D", corEscura: "#6C3737", corTexto: "#994D4D", voltarMobile: "voltar_cs.png", corHamburger: "#994D4D" },
+    "pre": { corPagina: "#FFF5F7", corPrimaria: "#E691A7", corEscura: "#D54267", corTexto: "#E691A7", voltarMobile: "voltar_rs.png", corHamburger: "#E691A7" },
+    "jd": { corPagina: "#f0f2f5", corPrimaria: "#6c757d", corEscura: "#495057", corTexto: "#6c757d", voltarMobile: "voltar_cin.png", corHamburger: "#6c757d" }
 };
 
 const BIBLIOTECA_CONTEUDO = {
-    "jd": { "jd": { t1: "Jogos em", t2: "Destaque", sub: "Recursos Especiais", rodape: "&copy; Pequenos Curiosos" } },
-    "pre": { "pre": { t1: "Pequenos", t2: "Curiosos", sub: "Atividades | Pré-Escolar", rodape: "&copy; Pequenos Curiosos" }, "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | Pré-Escolar", rodape: "&copy; Pequenos Curiosos" } },
-    "ano1": { "portugues": { t1: "Pequenos", t2: "Leitores", sub: "Português | 1º Ano", rodape: "&copy; Pequenos Leitores" }, "matematica": { t1: "Pequenos", t2: "Matemáticos", sub: "Matemática | 1º Ano", rodape: "&copy; Pequenos Matemáticos" }, "estudo": { t1: "Pequenos", t2: "Exploradores", sub: "Estudo do Meio | 1º Ano", rodape: "&copy; Pequenos Exploradores" }, "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 1º Ano", rodape: "&copy; Pequenos Curiosos" } },
-    "ano2": { "portugues": { t1: "Jovens", t2: "Leitores", sub: "Português | 2º Ano", rodape: "&copy; Jovens Leitores" }, "matematica": { t1: "Jovens", t2: "Matemáticos", sub: "Matemática | 2º Ano", rodape: "&copy; Jovens Matemáticos" }, "estudo": { t1: "Jovens", t2: "Investigadores", sub: "Estudo do Meio | 2º Ano", rodape: "&copy; Jovens Investigadores" }, "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 2º Ano", rodape: "&copy; Pequenos Curiosos" } },
-    "ano3": { "portugues": { t1: "Super", t2: "Leitores", sub: "Português | 3º Ano", rodape: "&copy; Super Leitores" }, "matematica": { t1: "Super", t2: "Matemáticos", sub: "Matemática | 3º Ano", rodape: "&copy; Super Matemáticos" }, "estudo": { t1: "Super", t2: "Cientistas", sub: "Estudo do Meio | 3º Ano", rodape: "&copy; Super Cientistas" }, "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 3º Ano", rodape: "&copy; Pequenos Curiosos" } },
-    "ano4": { "portugues": { t1: "Mestres da", t2: "Leitura", sub: "Português | 4º Ano", rodape: "&copy; Mestres da Leitura" }, "matematica": { t1: "Mestre dos", t2: "Números", sub: "Matemática | 4º Ano", rodape: "&copy; Mestre dos Números" }, "estudo": { t1: "Mestres", t2: "do Mundo", sub: "Estudo do Meio | 4º Ano", rodape: "&copy; Mestres do Mundo" }, "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 4º Ano", rodape: "&copy; Pequenos Curiosos" } }
+    "jd": {
+        "jd": { t1: "Jogos em", t2: "Destaque", sub: "Recursos Especiais", intro: "Descobre os nossos jogos mais populares!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" }
+    },
+    "pre": {
+        "pre": { t1: "Pequenos", t2: "Curiosos", sub: "Atividades | Pré-Escolar", intro: "Brinca com os números, as cores e as formas!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" },
+        "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | Pré-Escolar", intro: "Diverte-te com estes jogos especiais!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" }
+    },
+    "ano1": {
+        "portugues": { t1: "Pequenos", t2: "Leitores", sub: "Português | 1º Ano", intro: "Explora as letras, as palavras e diverte-te a ler!", rodape: "&copy; Pequenos Leitores - Recursos Educativos" },
+        "matematica": { t1: "Pequenos", t2: "Matemáticos", sub: "Matemática | 1º Ano", intro: "Explora os números e diverte-te a contar!", rodape: "&copy; Pequenos Matemáticos - Recursos Educativos" },
+        "estudo": { t1: "Pequenos", t2: "Exploradores", sub: "Estudo do Meio | 1º Ano", intro: "Explora o mundo à tua volta, o teu corpo e a natureza!!", rodape: "&copy; Pequenos Exploradores - Recursos Educativos" },
+        "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 1º Ano", intro: "Diverte-te com estes jogos especiais!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" }
+    },
+    "ano2": {
+        "portugues": { t1: "Jovens", t2: "Leitores", sub: "Português | 2º Ano", intro: "Explora as palavras, as frases e diverte-te a ler!", rodape: "&copy; Jovens Leitores - Recursos Educativos" },
+        "matematica": { t1: "Jovens", t2: "Matemáticos", sub: "Matemática | 2º Ano", intro: "Explora as operações e diverte-te a calcular!", rodape: "&copy; Jovens Matemáticos - Recursos Educativos" },
+        "estudo": { t1: "Jovens", t2: "Investigadores", sub: "Estudo do Meio | 2º Ano", intro: "Investiga o mundo, a natureza!!!", rodape: "&copy; Jovens Investigadores - Recursos Educativos" },
+        "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 2º Ano", intro: "Diverte-te com estes jogos especiais!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" }
+    },
+    "ano3": {
+        "portugues": { t1: "Super", t2: "Leitores", sub: "Português | 3º Ano", intro: "Explora os textos e diverte-te a ler!", rodape: "&copy; Super Leitores - Recursos Educativos" },
+        "matematica": { t1: "Super", t2: "Matemáticos", sub: "Matemática | 3º Ano", intro: "Explora as operações e diverte-te a calcular!", rodape: "&copy; Super Matemáticos - Recursos Educativos" },
+        "estudo": { t1: "Super", t2: "Cientistas", sub: "Estudo do Meio | 3º Ano", intro: "Explora o passado, a natureza e o corpo humano!", rodape: "&copy; Super Cientistas - Recursos Educativos" },
+        "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 3º Ano", intro: "Diverte-te com estes jogos especiais!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" }
+    },
+    "ano4": {
+        "portugues": { t1: "Mestres da", t2: "Leitura", sub: "Português | 4º Ano", intro: "Explora os livros e diverte-te a ler!", rodape: "&copy; Mestres da Leitura - Recursos Educativos" },
+        "matematica": { t1: "Mestre dos", t2: "Números", sub: "Matemática | 4º Ano", intro: "Resolve os desafios e torna-te um Mestre!", rodape: "&copy; Mestre dos Números - Recursos Educativos" },
+        "estudo": { t1: "Mestres", t2: "do Mundo", sub: "Estudo do Meio | 4º Ano", intro: "Explora a história, a geografia e os segredos do nosso país!", rodape: "&copy; Mestres do Mundo - Recursos Educativos" },
+        "jd": { t1: "Jogos em", t2: "Destaque", sub: "Desafios | 4º Ano", intro: "Diverte-te com estes jogos especiais!", rodape: "&copy; Pequenos Curiosos - Recursos Educativos" }
+    }
 };
 
 const JOGO_CONFIG = {
-    areaAtiva: "pre", 
-    anoAtivo: "pre",        
+    areaAtiva: "jd", 
+    anoAtivo: "jd",        
     caminhoIconsMenu: "../icons/", 
     caminhoIconsJogos: "iconjogos/",
-    caminhoImg: "../img/animaisdomesticos",
-    sons: { acerto: "sons/acerto.mp3", erro: "sons/erro.mp3", vitoria: "sons/vitoria.mp3" },
-    iconesMenu: { home: "home.png", pre: "iconpre.png", jd: "icondestaque.png", ano1: "icon1.png", ano2: "icon2.png", ano3: "icon3.png", ano4: "icon4.png", voltar: "voltar.png" },
-    links: { home: "/", pre: "/pre", jd: "/destaques", ano1: "/1", ano2: "/2", ano3: "/3", ano4: "/4" },
+    iconesMenu: {
+        home: "home.png", 
+        pre: "iconpre.png", 
+        jd: "icondestaque.png", 
+        ano1: "icon1.png", 
+        ano2: "icon2.png", 
+        ano3: "icon3.png", 
+        ano4: "icon4.png", 
+        voltar: "voltar.png"
+    },
+    links: {
+        home: "/", 
+        pre: "/pre",
+        jd: "/destaques",
+        ano1: "/1",
+        ano2: "/2",
+        ano3: "/3",
+        ano4: "/4"
+    },
+    // Resultados atualizados para escala de 0 a 10
     relatorios: [
         { min: 9, max: 10, titulo: "És um craque!", img: "taca_1.png" },
         { min: 7, max: 8, titulo: "Muito bem!", img: "taca_2.png" },
         { min: 5, max: 6, titulo: "Estás quase lá!", img: "taca_2.png" },
         { min: 0, max: 4, titulo: "Continua a tentar!", img: "taca_4.png" }
     ],
-    listaFases: [{
-        tituloFase: "ANIMAIS DOMÉSTICOS",
-        jogos: [{ nome: "Encontra o Par", desc: "Encontra o animal igual ao que aparece em destaque no topo.", icon: "abelha.png" }]
-    }]
+    listaFases: [
+        {
+            tituloFase: "DESTAQUES DO MÊS",
+            corEtiqueta: "#6c757d", 
+            jogos: [
+                { 
+                    nome: "Estoura-Balão", 
+                    desc: "Estoure desenhos correspondentes.", 
+                    icon: "balao.png", 
+                    link: "balao/", 
+                    cor: "#495057" 
+                },
+                { 
+                    nome: "Encontrar o Par", 
+                    desc: "Encontra os pares dos animais domésticos.", 
+                    icon: "par_animais.png", 
+                    link: "animaisdomesticos/", 
+                    cor: "#6c757d" 
+                }
+            ]
+        }
+    ]
 };
 
-const JOGO_CATEGORIAS = {
-    "animais": {
-        itens: [
-            { img: "elefante.png" }, { img: "caranguejo.png" }, { img: "coala.png" }, { img: "aguia.png" },
-            { img: "aranha.png" }, { img: "canguru.png" }, { img: "foca.png" }, { img: "abelha.png" }
-        ]
-    }
+// ==========================================
+// DADOS ESPECÍFICOS DO JOGO DE MEMÓRIA
+// ==========================================
+
+const DADOS_JOGO_MEMORIA = {
+    caminhoImagens: "img/animaisdomesticos/",
+    // Lista de ficheiros de imagem (devem existir na pasta)
+    itens: [
+        { id: 1, img: "cao.png", nome: "Cão" },
+        { id: 2, img: "gato.png", nome: "Gato" },
+        { id: 3, img: "coelho.png", nome: "Coelho" },
+        { id: 4, img: "hamster.png", nome: "Hamster" },
+        { id: 5, img: "peixe.png", nome: "Peixe" },
+        { id: 6, img: "passaro.png", nome: "Pássaro" },
+        { id: 7, img: "tartaruga.png", nome: "Tartaruga" },
+        { id: 8, img: "porquinho.png", nome: "Porquinho da Índia" }
+    ]
 };
