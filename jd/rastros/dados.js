3f7b7c7e5b0749445c74174cbe3c0af4f9db05cc
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-07-31 16:44:46 +0100

    Update dados.js

--- jd/rastros/dados.js
+++ jd/rastros/dados.js
@@ -80,10 +80,10 @@ const JOGO_CONFIG = {
         { id: "home", label: "Início", icon: "home.png", link: "/" },
         { id: "pre", label: "Pré-Escolar", icon: "iconpre.png", link: "/pre" },
         { id: "ano1", label: "1º Ano", icon: "icon1.png", link: "/1" },
-        { id: "ano2", label: "2º Ano", icon: "icon2.png", link: "../2" },
-        { id: "ano3", label: "3º Ano", icon: "icon3.png", link: "../3" },
-        { id: "ano4", label: "4º Ano", icon: "icon4.png", link: "../4" },
-        { id: "voltar", label: "Voltar", icon: "AUTO", link: "javascript:history.back()" } 
+        { id: "ano2", label: "2º Ano", icon: "icon2.png", link: "/2" },
+        { id: "ano3", label: "3º Ano", icon: "icon3.png", link: "/3" },
+        { id: "ano4", label: "4º Ano", icon: "icon4.png", link: "/4" },
+        { id: "voltar", label: "Voltar", icon: "AUTO", link: "/" } 
     ],
 
     instrucoes: {
