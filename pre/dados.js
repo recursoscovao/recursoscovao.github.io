69912546f674a2131ef6d5dd07550d338c0ccbdd
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-08-03 11:36:13 +0100

    Update dados.js

--- pre/dados.js
+++ pre/dados.js
@@ -57,9 +57,9 @@ const JOGO_CONFIG = {
             tituloFase: "CLICAR E IDENTIFICAR",
             corEtiqueta: "#E91E63", 
             jogos: [
-                { nome: "Encontra o Animal Doméstico", desc: "Encontra o animal igual.", icon: "encontraoanimal.png", link: "encontraoanimal/", cor: "#C2185B" },
-                { nome: "Encontra o Animal Selvagem", desc: "Encontra o animal igual", icon: "encontraoanimal1.png", link: "encontraoanimal_1/", cor: "#D32F2F" },
-                { nome: "Toupeira das Letras", desc: "Bata nas toupeiras corretas.", icon: "rato.png", link: "toupeira/", cor: "#E64A19" },
+                { nome: "Encontra o Animal", desc: "Encontra o animal Doméstico.", icon: "encontraoanimal.png", link: "encontraoanimal/", cor: "#C2185B" },
+                { nome: "Encontra o Animal", desc: "Encontra o animal Selvagem", icon: "encontraoanimal1.png", link: "encontraoanimal_1/", cor: "#D32F2F" },
+                { nome: "Sombras Mágicas", desc: "Qual é a sombra correta.", icon: "sombrasmagicas.png", link: "sombrasmagicas/", cor: "#E64A19" },
                 { nome: "Estoura-Balão", desc: "Estoure desenhos correspondentes.", icon: "balao.png", link: "balao/", cor: "#C2185B" },
                 { nome: "Letra Inicial", desc: "Qual é a primeira letra desse desenho?", icon: "lapis.png", link: "letra-in/", cor: "#D32F2F" },
                 { nome: "Toupeira das Letras", desc: "Bata nas toupeiras corretas.", icon: "rato.png", link: "toupeira/", cor: "#E64A19" }
