4083cec12aabf99b7d084a93cf2bedf0d8244781
jogoscovao <recursosdigitaiscovao@gmail.com>
2026-09-13 16:25:18 +0100

    Update dados.js

--- 1/pt/dados.js
+++ 1/pt/dados.js
@@ -62,7 +62,7 @@ const JOGO_CONFIG = {
             tituloFase: "SONS, LETRAS E FORMAS BÁSICAS",
             corEtiqueta: "#E91E63", 
             jogos: [
-                { nome: "Estoura-Balão", desc: "Estoure desenhos correspondentes.", icon: "balao.png", link: "balao/", cor: "#C2185B" },
+                { nome: "Grafismos", desc: "Liga a linha.", icon: "f1_jogo01pt.png", link: "f1_jogo01pt", cor: "#C2185B" },
                 { nome: "Letra Inicial", desc: "Qual é a primeira letra desse desenho?", icon: "lapis.png", link: "letra-in/", cor: "#D32F2F" },
                 { nome: "Toupeira das Letras", desc: "Bata nas toupeiras corretas.", icon: "rato.png", link: "toupeira/", cor: "#E64A19" },
                 { nome: "Estoura-Balão", desc: "Estoure desenhos correspondentes.", icon: "balao.png", link: "balao/", cor: "#C2185B" },
